ae17f3387028fa1ae6352f0df64afb1597355d00
潘正明 <panzhengming@juxinli.com>
2018-07-18 18:16:02 +0800

    init project

---
+++ squirrel/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com"
+	"log"
+)
+
+func main() {
+
+	// setup minio server info
+	endpoint := "192.168.200.20:9000"
+	accessKeyID := "1Y1P8ZMW7A4X8SXRD1DD"
+	secretAccessKey := "dhAUR7JKmdKcdFAcn6grRxnO7MuKZy8moMc+C9V+"
+	userSSL = false
+
+	
+}
